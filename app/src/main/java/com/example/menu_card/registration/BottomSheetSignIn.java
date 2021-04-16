24378d8e909b61ea0997f72261c7b9f36fa146f6
Abhiraj Kale <abhirajkale1806@gmail.com>
2021-04-16 21:28:29 +0530

    double click prevention

--- app/src/main/java/com/example/menu_card/registration/BottomSheetSignIn.java
+++ app/src/main/java/com/example/menu_card/registration/BottomSheetSignIn.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,8 +14,11 @@ import androidx.annotation.Nullable;
 import com.example.menu_card.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-public class BottomSheetSignIn extends BottomSheetDialogFragment {
+import java.util.Objects;
 
+public class BottomSheetSignIn extends BottomSheetDialogFragment {
+    TextView sign_up_btn;
+    static boolean isVisible = false;
 
     public BottomSheetSignIn() {
     }
@@ -23,6 +27,18 @@ public class BottomSheetSignIn extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.sign_in,container,false);
+
+        sign_up_btn = view.findViewById(R.id.sign_up_redirect);
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isVisible){
+                    isVisible = true;
+                    BottomSheetSignUp sign_up =  new BottomSheetSignUp();
+                    sign_up.show(Objects.requireNonNull(getActivity()).getSupportFragmentManager(),"TAG");
+                }
+            }
+        });
         return view;
 
 
--- app/src/main/java/com/example/menu_card/registration/BottomSheetSignUp.java
+++ app/src/main/java/com/example/menu_card/registration/BottomSheetSignUp.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +13,11 @@ import androidx.annotation.Nullable;
 import com.example.menu_card.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import java.util.Objects;
+
 public class BottomSheetSignUp extends BottomSheetDialogFragment {
+    TextView sign_in_btn;
+    static boolean isVisible = false;
 
     public BottomSheetSignUp() {
     }
@@ -21,6 +26,19 @@ public class BottomSheetSignUp extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.sign_up,container,false);
+
+        sign_in_btn = view.findViewById(R.id.sign_in_redirect);
+        sign_in_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isVisible){
+                    isVisible = true;
+                    BottomSheetSignIn sign_in =  new BottomSheetSignIn();
+                    sign_in.show(Objects.requireNonNull(getActivity()).getSupportFragmentManager(),"TAG");
+                }
+            }
+        });
+
         return view;
 
 
