c284a786f85d1d783249b7ac43f522b29571b398
lmnoob <yrohan740@gmail.com>
2021-04-21 09:05:47 +0530

    Added scroll to recent order page and added make order page

---
+++ app/src/main/java/com/example/menu_card/activity_make_order.java
@@ -0,0 +1,13 @@
+package com.example.menu_card;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_make_order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_order);
+    }
+}
