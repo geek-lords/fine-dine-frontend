263d644ac688c8eddeecdc144977aeb8d19f6c5d
Abhiraj Kale <abhirajkale1806@gmail.com>
2021-05-24 17:29:07 +0530

    progressBar at SplashScreen

--- app/src/main/java/com/example/menu_card/registration/SplashScreen.java
+++ app/src/main/java/com/example/menu_card/registration/SplashScreen.java
@@ -3,12 +3,16 @@ package com.example.menu_card.registration;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.menu_card.Common.common_methods;
@@ -24,11 +28,14 @@ import static com.example.menu_card.Common.common_methods.getKey;
 import static com.example.menu_card.registration.MainActivity.BASE_URL;
 
 public class SplashScreen extends AppCompatActivity {
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        progressBar = findViewById(R.id.progressBar_splashScreen);
 
         int SPLASH_DISPLAY_LENGTH = 2450;
         new Handler().postDelayed(() -> {
@@ -37,6 +44,7 @@ public class SplashScreen extends AppCompatActivity {
                 String restaurant_id = common_methods.getKey(SplashScreen.this, "restaurant_id");
                 String table_no = common_methods.getKey(SplashScreen.this, "table_no");
 
+                progressBar.setVisibility(View.VISIBLE);
                 getMenu(restaurant_id, result1 -> {
                     Intent intent = new Intent(SplashScreen.this, com.example.menu_card.order.activity_make_order.class);
                     intent.putExtra("menu", result1);
@@ -71,16 +79,20 @@ public class SplashScreen extends AppCompatActivity {
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 response -> {
+                    progressBar.setVisibility(View.INVISIBLE);
                     try {
                         callback.onSuccess(response);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                }, volleyError -> new AlertDialog.Builder(SplashScreen.this)
-                        .setTitle("Couldn't fetch menu")
-                        .setCancelable(true)
-                        .setMessage(common_methods._print_server_response_error(volleyError))
-                        .setPositiveButton("OK", (dialog, which) -> dialog.cancel()).show());
+                }, volleyError -> {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    new AlertDialog.Builder(SplashScreen.this)
+                            .setTitle("Couldn't fetch menu")
+                            .setCancelable(true)
+                            .setMessage(common_methods._print_server_response_error(volleyError))
+                            .setPositiveButton("OK", (dialog, which) -> dialog.cancel()).show();
+                });
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
